2e28dd069529595449b00600e4454e9c00e84d74
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2023-11-07 15:39:38 -0300

    Cria exemplo de Switch Case

--- 03 Comandos de decisão/code.java
+++ 03 Comandos de decisão/code.java
@@ -21,6 +21,29 @@ public class code{
         
         // Operação Condicional Ternária
         String res = nota >= media ? "Aprovado" : "Reprovado";
-        System.out.printf("Nota: %d%nMedia: %d%nResultado: %s", nota, media, res);
+        System.out.printf("Nota: %d%nMedia: %d%nResultado: %s\n\n", nota, media, res);
+
+
+        // Switch
+        int pos=6;
+
+        switch(pos){
+            case 1:
+                System.out.println("Primeiro Lugar");
+                break;
+            case 2:
+                System.out.println("Segundo Lugar");
+                break;
+            case 3:
+                System.out.println("Terceiro Lugar");
+                break;
+
+            case 4: case 5: case 6:
+                System.out.println("Premio de participacao");
+                break;
+            default:
+                System.out.println("Nao ganhou premio");
+                break;
+        }
     }
 }
