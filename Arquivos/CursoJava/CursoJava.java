63a7d92845a6e680461bb38687cda31c6c807606
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2023-11-07 14:55:07 -0300

    Altera o nome das pastas

--- Arquivos/CursoJava/CursoJava.java
+++
@@ -1,8 +0,0 @@
-public class CursoJava{
-    public static void main(String[] args){
-        //Diferentes tipos de Print
-        System.out.print("Olá mundo!\n");
-        System.out.println("Curso de Java");
-        System.out.printf("Nick: %s - Ano: %d", "Kaworii", 2023);
-    }
-}
