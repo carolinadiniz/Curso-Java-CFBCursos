e5d7f20c14b47de23cf36421c8e541fe9604f566
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2023-11-16 13:47:02 -0300

    Adiciona os códigos da aula 11 sobre classes

---
+++ 11 Diferença entre public e private/Jogador.java
@@ -0,0 +1,12 @@
+public class Jogador {
+
+    private int num = 0;
+    private int vidas=1;
+    
+    public Jogador(int num) {
+        this.num=num;
+        this.vidas = 3;
+        System.out.printf("Jogador numero %d criado!%n",num);
+    }
+
+}
