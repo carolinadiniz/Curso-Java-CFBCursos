d6c16a5384ddeb240f2750e8b65e7a219b71f9c7
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2023-11-25 12:57:14 -0300

    Adiciona o exercicio 1 da aula 18

---
+++ 18 Interface em Java/Exercicio 1/Animal.java
@@ -0,0 +1,4 @@
+public interface Animal {
+    public void emitirSom();
+    public void mover();
+}
---
+++ 18 Interface em Java/Exercicio 1/Cachorro.java
@@ -0,0 +1,9 @@
+public class Cachorro implements Animal {
+    public void emitirSom() {
+        System.out.println("Au au au!");
+    }
+    public void mover() {
+        System.out.println("O Cachorro esta andando...");
+    }
+
+}
