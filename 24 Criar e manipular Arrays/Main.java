a31e967581f18835bebf9541f9a716d2d16a1476
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2023-12-01 12:49:34 -0300

    Adiciona aula 24

---
+++ 24 Criar e manipular Arrays/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int[] num = new int[5];
+
+        num[0]=52;
+        num[1]=100;
+        num[2]=3;
+        num[3]=15;
+        num[4]=527;
+
+        for(int i=0; i<num.length ;i++) {
+            System.out.printf("%d - ", num[i]);
+        }
+
+    }
+}
